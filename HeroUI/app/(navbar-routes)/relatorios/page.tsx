255596cc3a4b38691c588a78ea0ddfaecd61b350
ItaloVasconcelos05 <italomiguel9705@gmail.com>
2025-10-08 21:19:11 -0300

    feat: Adicionei o MusicCard  no Relatório

--- HeroUI/app/(navbar-routes)/relatorios/page.tsx
+++ HeroUI/app/(navbar-routes)/relatorios/page.tsx
@@ -1,7 +1,11 @@
+"use client"
+import MusicCard from "@/components/blurred_card"
+
 export default function RelatoriosPage() {
     return(
         <div>
             <h1>Teste</h1>
+            <MusicCard></MusicCard>
         </div>
 
     )
