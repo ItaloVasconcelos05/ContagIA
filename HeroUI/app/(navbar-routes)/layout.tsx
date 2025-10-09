df95131cc83222264ab51960bec6891dc70d4196
ItaloVasconcelos05 <italomiguel9705@gmail.com>
2025-10-08 21:18:35 -0300

    refactor: Mudei o caminho do import

--- HeroUI/app/(navbar-routes)/layout.tsx
+++ HeroUI/app/(navbar-routes)/layout.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import {Providers} from "../providers";
-import {Navbar} from "../../components/navbar"
+import {Navbar} from "@/components/navbar"
 
 export default function NavLayout({children}: { children: React.ReactNode }) {
   return (
